64c377ee08886ac7de122e17b3a973eddf91a7e2
Badong <salvador.cecilio721@gmail.com>
2024-12-30 20:50:37 +0800

    gsgs

--- ugs_proj/settings.py
+++ ugs_proj/settings.py
@@ -224,15 +224,11 @@ DATABASE_URL =os.getenv('DATABASE_URL', None)
 
 if not DATABASE_URL:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'ugs_db',
-            'USER': 'root',
-            'PASSWORD':'Kinsee_06',
-            'HOST':'localhost',
-            'PORT':'3306',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 else:
     DATABASES = {
     #    'default': {
